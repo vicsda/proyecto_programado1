d5b227c97d058995d0f7173ba93baf6a68864965
David Coto <davox0503@gmail.com>
2022-05-15 18:15:55 -0600

    5/15/22 -> 6:15 PM

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@
 
                     Nota I: Las clases y archivos mas "interesantes" poseen una breve descripcion en sus .h/.cpp
 
+                    Nota II: El sistema ya posee elementos por defecto (Los especificados por el enunciado). Estos
+                    son elaborados por un constructor "especial" de testing en la clase 'ControlPrincipal'. Los datos
+                    de estos pueden ser accesidos en el codigo desde este mismo constructor "especial", o en el apartado
+                    de 'Reportes' en el programa.
+
+                    Nota III: Cuando se mencione en la interfaz "Oprima ENTER para continuar" a veces requerira que se
+                    oprima dos veces. Creemos que son problemas de compatibilidad entre CLion y la funcion getch() de
+                    <conio.h>. Sin embargo esto no es muy frecuente, ni tampoco afecta en la experiencia del programa de forma
+                    importante.
+
 */
 
 int main() {
